3e78509d39ef0f49795d8bbcdcb59ca5659d9925
YarnikeyZ <83335375+YarnikeyZ@users.noreply.github.com>
2023-05-12 18:19:54 +0500

    Added preloading of the sounds

--- soappybar.py
+++ soappybar.py
@@ -2,48 +2,57 @@ from tkinter import *
 import configparser
 from os.path import abspath as wd
 from os import listdir
-import simpleaudio as sa
+import simpleaudio
+
+config = configparser.ConfigParser()
+config.read(f'{wd("")}/soap_config.ini')
+
+def preloadSounds(sounds):
+    soundsLoaded = []
+    for sound in sounds:
+        soundName = sound.replace(".wav", "")
+        soundData = simpleaudio.WaveObject.from_wave_file(f"{config['btns']['folder']}\\{sound}")
+        soundsLoaded.append((soundName, soundData))
+    return soundsLoaded
+
+def addSoundButton(window, sound, cxr):
+    btn = Button(master=window,
+                 text=sound[0],
+                 command=lambda: sound[1].play()
+    )
+
+    btn.configure(width=int(config['btns']['geometry'].split('x')[0])*2,
+                  height=int(config['btns']['geometry'].split('x')[1]),
+                  activebackground=config['colors']['btn_abg'],
+                  background=config['colors']['btn_bg'],
+                  highlightbackground=config['colors']['btn_bd'],
+                  foreground=config['colors']['btn_text'],
+                  activeforeground=config['colors']['btn_atext'],
+                  font=(config['font']['name'], int(config['font']['size']), config['font']['mode']),
+    )
+
+    cxr[0] += 1
+    if cxr[0]//int(config['btns']['columns']) == 1:
+        btn.grid(column=cxr[0], row=cxr[1])
+        cxr = [0, cxr[1]+1]
+    else:
+        btn.grid(column=cxr[0], row=cxr[1])
+    return cxr
 
 def main():
-    # loading config part
-    config = configparser.ConfigParser()
-    config.read(f'{wd("")}/soap_config.ini')
-
-    # openning & configuring window part
-    window = Tk()
-    window.title(config['window']['title'])
-    window.configure(bg=config['colors']['window_bg'])
-
-    # buttons \w sounds part
-    cxr = [0, 0] # column x row
-    sounds = listdir(config['btns']['folder'])
-    sounds.sort()
-    for s in range(0, len(sounds)):
-        btn = Button(master=window,
-                     text=sounds[s].replace(".wav", ""),
-                     command=lambda sound=sounds[s]: sa.WaveObject.from_wave_file(f"{config['btns']['folder']}/{sound}").play()
-        )
-
-        btn.configure(width=int(config['btns']['geometry'].split('x')[0])*2,
-                      height=int(config['btns']['geometry'].split('x')[1]),
-                      activebackground=config['colors']['btn_abg'],
-                      background=config['colors']['btn_bg'],
-                      highlightbackground=config['colors']['btn_bd'],
-                      foreground=config['colors']['btn_text'],
-                      activeforeground=config['colors']['btn_atext'],
-                      font=(config['font']['name'], int(config['font']['size']), config['font']['mode']),
-        )
-
-        # grid handeling
-        cxr[0] += 1
-        if cxr[0]//int(config['btns']['columns']) == 1:
-            btn.grid(column=cxr[0], row=cxr[1])
-            cxr = [0, cxr[1]+1]
-        else:
-            btn.grid(column=cxr[0], row=cxr[1])
-
-
     try:
+        window = Tk()
+        window.title(config['window']['title'])
+        window.configure(bg=config['colors']['window_bg'])
+
+        sounds = listdir(config['btns']['folder'])
+        sounds.sort()
+        sounds = preloadSounds(sounds)
+
+        cxr = [0, 0]
+        for sound in sounds:
+            cxr = addSoundButton(window, sound, cxr)
+
         window.mainloop()
     except KeyboardInterrupt:
         exit()
